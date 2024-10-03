5937d619eee9712fec538ca6ed2393fabc8ce29f
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-03 20:05:40 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,20 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
+
 def pascal_triangle(n):
     # Return an empty list if n is less than or equal to 0
     if n <= 0:
@@ -24,50 +41,3 @@ def pascal_triangle(n):
         triangle.append(row)
     
     return triangle
-
-# Example usage:
-print(pascal_triangle(5))
-
-
-
-#def pascal_triangle(n):
-   # if n <= 0:
-    #    return []
-    
-  #  triangle = []
-    
-   # for i in range(n):
-       # row = [1] * (i + 1)  # Start with a row of 1's
-     #   for j in range(1, i):  # Calculate the values in the middle
-       #     row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-      #  triangle.append(row)
-    
-  #  return triangle
-
-# Example usage:
-#result = pascal_triangle(5)
-#print(result
-     )
-
-
-
-#!/usr/bin/python3
-"""
-0. Pascal's Triangle
-"""
-
-
-#def pascal_triangl(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
-  #  res = []
-  #  if n > 0:
-   #     for i in range(1, n + 1):
-          #  level = []
-         #   C = 1
-         #   for j in range(1, i + 1):
-          #      level.append(C)
-           #     C = C * (i - j) // j
-          #  res.append(level)
-   # return re
