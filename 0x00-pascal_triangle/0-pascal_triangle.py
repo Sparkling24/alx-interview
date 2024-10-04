eca59c801d0f0e8aa731bcaa92b0b6855d23a83a
Sparkling24 <chimexdguy2020@gmail.com>
2024-10-03 18:03:34 -0700

    1242

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,41 +1,14 @@
-def pascal_triangle(n):
-    if n <= 0:
-        return []
-    
-    triangle = []
-    
-    for i in range(n):
-        row = [1] * (i + 1)  # Start with a row of 1's
-        for j in range(1, i):  # Calculate the values in the middle
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        triangle.append(row)
-    
-    return triangle
-
-# Example usage:
-result = pascal_triangle(5)
-print(result
-     )
-
-
-
-#!/usr/bin/python3
-"""
-0. Pascal's Triangle
-"""
-
-
-def pascal_triangl(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return re
+define pascal_triangle(n)
+       a=[[] for i in range(n)]
+       for i in range(n):
+       for j in range(i+1):
+           if(j<i):
+               if (j==0):
+                   a[i].append(1)
+                else:
+                    a[i].append(a[i-1][j]+a[i-1][j-1])
+            elif(j==i):
+                a[i].append(1)
+        return a
+n=5
+print(pascal_triangle(n)
