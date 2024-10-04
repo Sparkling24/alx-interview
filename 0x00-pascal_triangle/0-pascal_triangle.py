b65f4816b02de4644cc6deb9c7f37335696d6c4b
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-04 03:40:24 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangl
+e
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+                                                 if __name__ == "__main__":                                          print_triangle(pascal_triangle(5))
 define pascal_triangle(n)
        a=[[] for i in range(n)]
        for i in range(n):
